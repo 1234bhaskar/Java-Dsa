aef025a07e6e532c19fc141005833fb6788284a3
1234bhaskar <bhaskar2021chand@gmail.com>
2023-08-28 06:17:53 +0530

    Linked list Questions/Merge 2sorted list

--- LinkedList/LL.java
+++ LinkedList/LL.java
@@ -130,6 +130,33 @@ public class LL {
         tail.next=null;
     }
 
+    //2.Merge 2 sorted arrays
+    public static LL merge(LL first,LL second){
+        Node f=first.head;
+        Node s=second.head;
+
+        LL ans=new LL();
+        while(s!=null && f!=null){
+             if(s.value>f.value){
+                 ans.insertLast(f.value);
+                 f=f.next;
+             }else{
+                 ans.insertLast(s.value);
+                 s=s.next;
+             }
+        }
+
+        while (f !=null){
+            ans.insertLast(f.value);
+            f=f.next;
+        }
+        while(s!=null){
+            ans.insertLast(s.value);
+            s=s.next;
+        }
+        return ans;
+    }
+
 
 
     private class Node{
--- LinkedList/QuestionsLL.java
+++ LinkedList/QuestionsLL.java
@@ -2,8 +2,8 @@ package javaBasics.LinkedList;
 
 public class QuestionsLL {
     public static void main(String[] args) {
-          LL list=new LL();
-        list.insertLast(1);
+         LL list=new LL();
+        /*list.insertLast(1);
         list.insertLast(1);
         list.insertLast(2);
         list.insertLast(3);
@@ -12,7 +12,23 @@ public class QuestionsLL {
 
         list.display();
         list.duplicates();
-        list.display();
+        list.display();*/
 
+        //merge 2 sorted list
+
+        LL first=new LL();
+         LL second= new LL();
+
+         first.insertLast(1);
+        first.insertLast(3);
+        first.insertLast(5);
+
+       second.insertLast(1);
+        second.insertLast(2);
+        second.insertLast(9);
+        second.insertLast(14);
+
+        LL ans=LL.merge(first,second);
+        ans.display();
     }
 }
